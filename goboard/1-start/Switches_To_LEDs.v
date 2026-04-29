718e8217587c03a955bfe77610834cb21cd502e6
Piyush Saurabh <saurabh.piyush2807@gmail.com>
2026-04-28 22:15:58 -0700

    add comments for better readability

--- goboard/1-start/Switches_To_LEDs.v
+++ goboard/1-start/Switches_To_LEDs.v
@@ -1,13 +1,20 @@
 module Switches_To_LEDs
- (input  i_Switch_1,
-  input  i_Switch_2,
-  input  i_Switch_3,
-  input  i_Switch_4,
-  output o_LED_1,
-  output o_LED_2,
-  output o_LED_3,
-  output o_LED_4);
+ (
+    // Inputs
+    input  i_Switch_1,
+    input  i_Switch_2,
+    input  i_Switch_3,
+    input  i_Switch_4,
 
+    // Outputs
+    output o_LED_1,
+    output o_LED_2,
+    output o_LED_3,
+    output o_LED_4
+  );
+
+  // Continuous assignment
+  // There is no clock. It is like physical wiring on breadboard
   assign o_LED_1 = i_Switch_1;
   assign o_LED_2 = i_Switch_2;
   assign o_LED_3 = i_Switch_3;

0809c9037e3d4c45e8772a593cf07a803dec3347
Piyush Saurabh <saurabh.piyush2807@gmail.com>
2026-05-05 22:29:55 -0700

    add more verbose comments

--- goboard/1-start/Switches_To_LEDs.v
+++ goboard/1-start/Switches_To_LEDs.v
@@ -1,18 +1,22 @@
 module Switches_To_LEDs
  (
     // Inputs
+    // default size: 1 bit
     input  i_Switch_1,
     input  i_Switch_2,
     input  i_Switch_3,
     input  i_Switch_4,
 
     // Outputs
+    // default size: 1 bit
     output o_LED_1,
     output o_LED_2,
     output o_LED_3,
     output o_LED_4
   );
 
+  // Logic of the design
+  
   // Continuous assignment
   // There is no clock. It is like physical wiring on breadboard
   assign o_LED_1 = i_Switch_1;
